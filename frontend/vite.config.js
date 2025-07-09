df9609e3a152f2b9d9f419f2219d20ea07d1ec72
John-max-spec <B01734504@studentmail.uws.ac.uk>
2025-07-09 03:07:43 +0100

    描述此次提交的更改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
-  
-    
-    base: mode === 'production' ? '/todo/' : '/',
+    base: './', // 设置相对路径
   };
 });
